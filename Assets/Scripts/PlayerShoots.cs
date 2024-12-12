254203d609ec09f1621da7a0978b74f0d8e4adb7
Alex <aburch280@gmail.com>
2024-12-12 11:00:20 -0700

    Final Deliverable of Final Project

---
+++ Assets/Scripts/PlayerShoots.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public Transform GunPoint;
+    public GameObject BulletTemplate;
+    public float shootPower = 500;
+
+    public InputActionReference trigger;
+    public AudioClip gunShotSFX;
+
+    void Start()
+    {
+        trigger.action.performed += Shoot;
+    }
+
+    void Shoot(InputAction.CallbackContext _)
+    {
+        GameObject newBullet = Instantiate(BulletTemplate, GunPoint.position, transform.rotation);
+        newBullet.GetComponent<Rigidbody>().AddForce(transform.forward * shootPower);
+
+        GetComponent<AudioSource>().PlayOneShot(gunShotSFX);
+
+        // Destroy(newBullet, 5);
+    }
+}
